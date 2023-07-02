c124231f4eb79c82bc46405c4d645bb2fea70777
Axel Lodyga <axel@dawolke.de>
2023-07-02 19:35:33 +0200

    client for usb reader acr122u

---
+++ client/client_acr122u.py
@@ -0,0 +1,98 @@
+import time
+import sys
+import os
+from bottle import route
+import bottle
+from bottle import response
+from lib.acr122u import nfc
+import math
+import struct
+
+from lib.acr122u import utils, option, error, nfc
+
+
+
+##
+## RFID
+##
+class RFID:
+    def __init__(self):
+        self.reader = None
+        # self.reader.info()
+        # self.reader.authentication(0x01, 0x61, 0x01)
+    def read(self):
+        self.reader = nfc.Reader()
+        self.reader.authentication(0x01, 0x61, 0x01)
+        token_id, text = None, None
+        for i in range(3):
+            token_id, text = self.reader.read_no_block()
+            if token_id is not None:
+                text = text.strip()
+                print("successful read", text)
+                break
+            time.sleep(1)
+        return token_id, text
+
+    def write(self, text):
+        self.reader = nfc.Reader()
+        self.reader.authentication(0x01, 0x61, 0x01)
+        token_id, output = None, None
+        for i in range(3):
+            token_id, output = self.reader.write_no_block(text)
+            if token_id is not None:
+                break
+            time.sleep(1)
+        return token_id, output
+
+
+##
+## WEB API
+##
+def btl_enable_cors(fn):
+    def _enable_cors(*args, **kwargs):
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+        response.headers[
+            'Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token, X-Test'
+        if bottle.request.method != 'OPTIONS':
+            return fn(*args, **kwargs)
+
+    return _enable_cors
+
+
+@route('/rfid/read/', method=['OPTIONS', 'GET'])
+@btl_enable_cors
+def route_rfid_read():
+    token_id, text = rfid.read()
+    if token_id is None:
+        return ""
+    return "%s\t%s" % (token_id, text)
+
+
+@route('/rfid/write/<value>', method=['OPTIONS', 'GET'])
+@btl_enable_cors
+def route_rfid_write(value=""):
+    if value != "":
+        token_id, text = rfid.write(value)
+        if token_id is not None:
+            return "OK"
+    return "Failed"
+
+
+##
+## Commandline
+##
+if __name__ == "__main__":
+    rfid = RFID()
+
+    if len(sys.argv) == 1:
+         bottle.run(host='127.0.0.1', port=8080)
+    else:
+        if sys.argv[1] == "write":
+            print("RFID TAG AUFLEGEN")
+            print(rfid.write(sys.argv[2]))
+        elif sys.argv[1] == "read":
+            print("RFID TAG AUFLEGEN")
+            print(rfid.read())
+        else:
+            print("Unknown Option '%s'" % sys.argv[1])
--- client/test.py
+++ client/test.py
@@ -1,5 +1,98 @@
-from lib.er302 import ER302
-device = ER302('/dev/tty.usbserial-0001', 115200)
-print('lets test!')
-print(device.send_receive(0x10c,b'\0x01'))
-print(device.select())
+from lib.acr122u import nfc
+import math
+import struct
+
+from lib.acr122u import utils, option, error
+reader = nfc.Reader()
+DEFAULT_KEYS = [
+    [0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+    [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF],
+    [0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5],
+    [0xb0, 0xb1, 0xb2, 0xb3, 0xb4, 0xb5],
+    [0x4d, 0x3a, 0x99, 0xc3, 0x51, 0xdd],
+    [0x1a, 0x98, 0x2c, 0x7e, 0x45, 0x9a],
+    [0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf],
+    [0xd3, 0xf7, 0xd3, 0xf7, 0xd3, 0xf7],
+    [0xaa, 0xbb, 0xdd, 0xcc, 0xee, 0xff],
+]
+reader.load_authentication_data(0x01, DEFAULT_KEYS[1])
+reader.authentication(0x00, 0x61, 0x01)
+# reader.get_uid()
+# reader.info()
+# reader.get_picc_version()
+
+def write_txt(r, position, data):
+    data = bytes(data, 'ascii')
+    
+    for i in range(0, len(data), 16):
+        start = math.floor((i) / 16)+position
+        data_part = data[i: i + 16]
+        while len(data_part) < 16:
+            data_part += bytes([0])
+        length = len(data_part)
+        # if start
+        # print("Position: ",start)
+        # print("Number: ",length)
+        # print("Data: ",data_part)
+        if start >= position+3:
+            raise error.InstructionFailed(f"OUT OF BLOCK ")
+        r.update_binary_blocks(start,length,data_part)
+
+def write(r, position, number, data):
+    while number >= 16:
+        write_16(r, position, 16, data)
+        number -= 16
+        position += 1
+
+def write_16(r, position, number, data):
+    r.update_binary_blocks(position, number, data)
+
+
+def read(r, position, number):
+    result = []
+    while number >= 16:
+        result.extend(read_16(r, position, 16))
+        number -= 16
+        position += 1
+    return result
+
+def test_keys(r, sector=0, keys=DEFAULT_KEYS):
+        """Testet ein Array potenzieller Schlüssel, um den richtigen zu finden.
+
+        Schlüsselwort-Argumente:
+        sector -- der Sektorindex (Standard: 0)
+        keys -- die zu testenden Schlüssel (Standard: DEFAULT_KEYS)
+
+        """
+        for key in keys:
+            try:
+                print(r.authentication(sector, 0x60, 0x01))
+                print("WORKING A", [f"{byte:02X}" for byte in key])
+                print("READ: ", [f"{byte:02X}" for byte in read(reader, 0x01, 0x20)])
+            except:
+                print('A-')    
+
+        for key in keys:
+            try:
+                r.load_authentication_data(0x01,key)
+                print(r.authentication(sector, 0x61, 0x01))
+                print("WORKING B",[f"{byte:02X}" for byte in key])
+                print("READ: ", [f"{byte:02X}" for byte in read(reader, 0x01, 0x20)])
+            except:
+                print('B-')    
+
+def read_16(r, position, number):
+    return r.read_binary_blocks(position, number)
+
+
+
+
+text = "U:axell;4219bcc4fce5cb355108"
+#print( write_txt(reader,0x01,text))
+#write(reader, 0x02, 0x20, [0x00 for i in range(0,16)])
+read_data = reader.read_no_block()
+# reader.print_data(read_data)
+# print(bytes(read_data[1]).decode('ascii'))
+print(read_data)
+# test_keys(reader,0x00)    
+# print("READ: ", read(reader, 0x01, 0x20))
