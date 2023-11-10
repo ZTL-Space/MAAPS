717e7c39332e75440aec9663596442cc34ae5d67
Axel Lodyga <axel@dawolke.de>
2023-11-10 14:52:02 +0100

    Docker version, working with composer

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
+
+WORKDIR /server
+
+COPY ./server/requirements.txt ./
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY ./server .
--- server/maaps/settings.py
+++ server/maaps/settings.py
@@ -54,9 +54,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'maaps.wsgi.application'
 
 DATABASES = {
-    'default': {
+    'file': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
